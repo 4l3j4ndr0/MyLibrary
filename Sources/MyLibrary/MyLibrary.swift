6222e745eb080807119a2db01fb0e7741fff0e56
4l3j4ndr0 <alejandro.casta3447@gmail.com>
2021-03-29 18:00:39 -0500

    cambios

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,11 +1,14 @@
 public struct MyLibrary {
-    var text = "Hola papi"
-    
-    public init(){
-        
+    let API_URL     = "https://api.secure.epayco.xyz"
+    let SECURE_URL  = "String"
+    var PUBLIC_KEY  : String
+    var PRIVATE_KEY : String
+    public init(publicKey: String, privateKey: String){
+        self.PUBLIC_KEY = publicKey
+        self.PRIVATE_KEY = privateKey
     }
     
     public func prueba() -> String {
-        return self.text
+        return self.PUBLIC_KEY
     }
 }
